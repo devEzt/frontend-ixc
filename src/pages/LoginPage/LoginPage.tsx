740343a43caf7ad8e060288fd6d49036dea5f055
Alexandre <eztalenda@gmail.com>
2024-08-18 04:08:36 -0300

    feat: Adiciona componentes do material ui para estilização inicial da tela de login e execução do axios para conexão com o backend.

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,21 +1,66 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { Container, TextField, Button, Box, Typography, Paper } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 export const LoginPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
 
-  const handleLogin = async () => {
-    navigate('/') // ficará assim até a conclusão da pagina de chat e as demais regras.
+  const handleLogin = async (event: any) => {
+    event.preventDefault()
+    try {
+      const response = await axios.post('http://localhost:5000/api/users/login', {
+        email,
+        password,
+      })
+      const { token } = response.data
+      localStorage.setItem('token', token)
+      navigate('/chat')
+    } catch (error) {
+      console.error('Login failed:', error)
+    }
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={6} sx={{ mt: 8, p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Box component="form" sx={{ mt: 1 }}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} onClick={handleLogin}>
+            Login
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
   )
 }
